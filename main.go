fd2374dc8e5d06de6bf219dc3d31b6a72d3c12e2
akyetisir <ahmet.yetisir@outlook.com>
2024-11-10 00:07:36 +0100

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+/*
+package main
+
+import "fmt"
+
+func main() {
+	card := newCard()
+
+	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
+*/
+package main
+     
+func main() {
+	printState()
+}
